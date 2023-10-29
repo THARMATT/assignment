907216045e84c61a392f0b2443d430f1607aab48
THARMATT <nigamsharma@gmail.com>
2023-10-29 20:34:12 +0530

    assignment

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import BackgroundImage from '../components/BackgroundImage'
+import Header from '../components/Header'
 
-import Signup from './Signup'
-function Login() {
+function Signup() {
   return (
-   <></>
+    <div className=''>
+      <BackgroundImage/>
+      <div className="absolute top-0 left-0 bg-opacity-50 bg-black h-screen w-screen flex items-center justify-center text-white">
+        <Header className=''/>
+      <div className="flex align-items justify-center ">
+        <div className="text flex flex-col">
+            <h1>Lets Explore the Beast</h1>
+            <h4>For making you feel Better</h4>
+            <h6>Ready?Enter your Email</h6>
+        </div>
+        <div className="form gap-1">
+            <input type='email' placeholder='Email Address' name='email'/>
+            <input type='password' placeholder='Password' name='password'/>
+            <button>Let's Begin</button>
+            <button>Login</button>
+        </div>
+      </div>
+    </div></div>
   )
 }
 
-export default Login
+export default Signup
